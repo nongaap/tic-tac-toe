a055eaa0658274481f1247a5e4aa8d9fe49c5fbe
Mike Puangmalai <mike@nongaap.com>
2016-12-06 14:37:44 -0800

    initial server.js file added with cluster mode and api route added.

---
+++ Server/server.js
@@ -0,0 +1,54 @@
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+
+  // Count the machine's CPUs
+  var cpuCount = require('os').cpus().length;
+
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+  }
+
+  // Listen for dying workers
+  cluster.on('exit', function (worker) {
+
+    console.log('Worker died :(', worker.id);
+    cluster.fork();
+
+    });
+
+} else {
+
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var api = require('./routes/api');
+
+var port = 3000;
+
+var app = express();
+
+var server = app.listen(port, function() {
+  console.log('listening on port 3000');
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+app.use('/api', api);
+
+app.get('/', function (req, res) {
+	res.status(200).send('Hi. Tic Tac Toe Homepage');
+});
+
+app.all('*', function (req, res) {
+  res.status(404).send('Nothing Here');
+});
+
+module.exports = server;
+}
