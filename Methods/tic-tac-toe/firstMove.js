88f812c41005261452c010580449fc9d3fbeabd8
Mike Puangmalai <mike@nongaap.com>
2016-12-05 18:04:07 -0800

    refactored firstMove function to accept a board array as a parameter.

--- Methods/tic-tac-toe/firstMove.js
+++ Methods/tic-tac-toe/firstMove.js
@@ -2,14 +2,15 @@
 
 /*
 firstMove function returns the position (assuming the tic-tac-toe board is numbered 0-8) a player should take for their first move.
-If no argument is passed, the function assumes it is making the first move in the game.
-If an argument is passed, the function assumes it is making the secomd move in the game. 
+If an empty board is passed in, the function assumes it is making the first move in the game.
 */
 
 function firstMove(position){
 
-	return position === 4 || position === undefined ? 0 : 4
+	return position.indexOf('X') === 4 || position.indexOf('X') === -1 ? 0 : 4
 
 }
 
 module.exports = firstMove;
+
+console.log(firstMove(['','','','','','','','','']));
