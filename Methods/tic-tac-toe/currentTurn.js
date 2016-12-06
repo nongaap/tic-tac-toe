3ee7f500b92be4260df8198af6655ff4329a17bb
Mike Puangmalai <mike@nongaap.com>
2016-12-05 16:27:51 -0800

    add descriptive comment to currentTurn.js file

--- Methods/tic-tac-toe/currentTurn.js
+++ Methods/tic-tac-toe/currentTurn.js
@@ -1,9 +1,7 @@
 "use strict";
 
 /*
-firstMove function returns the position (assuming the tic-tac-toe board is numbered 0-8) a player should take for their first move.
-If no argument is passed, the function assumes it is making the first move in the game.
-If an argument is passed, the function assumes it is making the secomd move in the game. 
+currentTurn function determine if it is X or O turn given a tic-tac-toe array. 
 */
 
 function currentTurn(board){
