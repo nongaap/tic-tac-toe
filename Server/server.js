a1a4f8fec2c539584be10ef330c23149c9e61a8f
Mike Puangmalai <mike@nongaap.com>
2016-12-10 22:32:02 -0800

    server updated to allow cross origin for api route.

--- Server/server.js
+++ Server/server.js
@@ -41,6 +41,13 @@ app.use(cookieParser());
 
 app.use(bodyParser.json());
 
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Method', 'GET, POST, HEAD, OPTIONS, PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
+  next();
+});
+
 app.use('/api', api);
 
 app.get('/', function (req, res) {
